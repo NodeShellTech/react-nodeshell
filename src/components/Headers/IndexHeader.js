02812648f71dbb64964268667794640b86df2ed2
Sachin Ojha <sachin.ojha@thoughts2binary.com>
2024-01-18 22:31:19 +0530

    chore(template): add dummy template for the website

---
+++ src/components/Headers/IndexHeader.js
@@ -0,0 +1,48 @@
+import { STRINGS } from "./strings";
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+
+// core components
+
+function IndexHeader() {
+  return (
+    <>
+      <div
+        className="page-header section-dark"
+        style={{
+          backgroundImage:
+            "url(" + require("assets/img/antoine-barres.jpg") + ")",
+        }}
+      >
+        <div className="filter" />
+        <div className="content-center">
+          <Container>
+            <div className="title-brand">
+              <h1 className="presentation-title">{STRINGS.NODESHELL}</h1>
+              <div className="fog-low">
+                <img alt="..." src={require("assets/img/fog-low.png")} />
+              </div>
+              <div className="fog-low right">
+                <img alt="..." src={require("assets/img/fog-low.png")} />
+              </div>
+            </div>
+            <h2 className="presentation-subtitle text-center">
+              {STRINGS.CAPTION}
+            </h2>
+          </Container>
+        </div>
+        <div
+          className="moving-clouds"
+          style={{
+            backgroundImage: "url(" + require("assets/img/clouds.png") + ")",
+          }}
+        />
+        <h6 className="category category-absolute">{STRINGS.CODED_BY}</h6>
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;
---
+++ src/components/Headers/strings.js
@@ -0,0 +1,6 @@
+export const STRINGS = {
+  NODESHELL: "NodeShell",
+  CAPTION:
+    "Innovative software solutions, turning ideas into reality with expertise in full-stack development, web, and mobile applications.",
+  CODED_BY: "Designed and coded by NodeShell",
+};
